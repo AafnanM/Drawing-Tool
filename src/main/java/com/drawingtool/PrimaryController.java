9528944806440f47155ae7b29e4ade8deeaea5fd
Aafnan M <aafnanm13@gmail.com>
2023-11-19 21:12:03 -0500

    Completed basic drawing functionality

--- src/main/java/com/drawingtool/PrimaryController.java
+++ src/main/java/com/drawingtool/PrimaryController.java
@@ -1,9 +1,64 @@
 package com.drawingtool;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.util.ResourceBundle;
+import java.net.URL;
 
-public class PrimaryController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.TextField;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.canvas.Canvas;
+
+public class PrimaryController implements Initializable {
+    @FXML
+    private ColorPicker colorpicker;
+    @FXML
+    private TextField bsize;
+    @FXML
+    private Canvas canvas;
+    
+    GraphicsContext brushTool;
+
+    private String selectedTool = "";
+    private int selectedBrush = 0;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        brushTool = canvas.getGraphicsContext2D();
+
+
+        canvas.setOnMouseDragged(e -> {
+            double size = Double.parseDouble(bsize.getText());
+            double x = e.getX() - size/2;
+            double y = e.getY() - size/2;
+
+            if (selectedBrush != 0 && !bsize.getText().isEmpty()) {
+                brushTool.setFill(colorpicker.getValue());
+                brushTool.fillRoundRect(x, y, size, size, size, size);
+            }
+        });
+    }
+
+    @FXML
+    public void newCanvas(ActionEvent e) {
+        TextField getCanvasWidth = new TextField();
+        getCanvasWidth.setPromptText("Width");
+        getCanvasWidth.setPrefWidth(500);
+        getCanvasWidth.setAlignment(Pos.CENTER);
+        TextField getCanvasHeight = new TextField();
+        getCanvasHeight.setPromptText("Height");
+        getCanvasHeight.setPrefWidth(500);
+        getCanvasHeight.setAlignment(Pos.CENTER);
+    }
+
+    @FXML
+    private void brushSelected(ActionEvent e) {
+        selectedBrush = 1;
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
