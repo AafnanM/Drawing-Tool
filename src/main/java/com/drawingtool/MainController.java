68dde4079a5c0c42bf8416bd8008aec42839893b
Aafnan M <aafnanm13@gmail.com>
2023-11-21 17:06:24 -0500

    Added second colorpicker and completed new canvas

--- src/main/java/com/drawingtool/MainController.java
+++ src/main/java/com/drawingtool/MainController.java
@@ -9,18 +9,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.Cursor;
 import javafx.scene.canvas.Canvas;
 
-public class MainController implements Initializable {
+public class MainController extends App implements Initializable {
     @FXML
-    private ColorPicker colorpicker;
+    private ColorPicker colorpickerPrimary;
+    @FXML
+    private ColorPicker colorpickerSecondary;
+    @FXML
+    private Slider bsizeSlider;
     @FXML
     private TextField bsize;
     @FXML
@@ -49,24 +56,68 @@ public class MainController implements Initializable {
     GraphicsContext brushTool;
 
     private String selectedTool = "";
-    private int selectedBrush = 0;
+    private int selectedBrush = 1;
+    private double mouseAnchorX;
+    private double mouseAnchorY;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         brushTool = canvas.getGraphicsContext2D();
 
-
+        //  Brush tool
         canvas.setOnMouseDragged(e -> {
             double size = Double.parseDouble(bsize.getText());
             double x = e.getX() - size/2;
             double y = e.getY() - size/2;
 
-            if (selectedBrush != 0 && !bsize.getText().isEmpty()) {
-                brushTool.setFill(colorpicker.getValue());
+            if (selectedTool == "brush" && !bsize.getText().isEmpty()) {
+                brushTool.setFill(colorpickerPrimary.getValue());
+                //if (selectedBrush == 1) {
                 brushTool.fillRoundRect(x, y, size, size, size, size);
+                //}
             }
         });
 
+        //  Change brush size
+        bsizeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            bsize.setText(Integer.toString(newValue.intValue()));
+        });
+        bsize.textProperty().addListener((observable) -> {
+            if (isValid(bsize.getText())) {
+                if (Integer.parseInt(bsize.getText()) > 100)
+                    bsize.setText("100");
+                else if (Integer.parseInt(bsize.getText()) < 1)
+                    bsize.setText("1");
+                bsizeSlider.setValue((double)Double.parseDouble(bsize.getText()));
+            }
+            else {
+                bsize.setText("1");
+                bsizeSlider.setValue(1.0);
+            }
+        });
+
+        //  Hand tool
+        canvasPane.setOnMousePressed(mouseEvent -> {
+            if (selectedTool == "hand") {
+                mouseAnchorX = mouseEvent.getX();
+                mouseAnchorY = mouseEvent.getY();
+                scene.setCursor(Cursor.CLOSED_HAND);
+            }
+        });
+        canvasPane.setOnMouseDragged(mouseEvent -> {
+            if (selectedTool == "hand") {
+                canvasPane.setLayoutX(mouseEvent.getSceneX() - mouseAnchorX);
+                canvasPane.setLayoutY(mouseEvent.getSceneY() - mouseAnchorY);
+                scene.setCursor(Cursor.CLOSED_HAND);
+            }
+        });
+        canvasPane.setOnMouseReleased(mouseEvent -> {
+            if (selectedTool == "hand") {
+                scene.setCursor(Cursor.OPEN_HAND);
+            }
+        });
+
+        //  Create new canvas
         canvasWidth.textProperty().addListener((observable) -> {
             changeDim();
         });
@@ -76,15 +127,36 @@ public class MainController implements Initializable {
         canvasColorpicker.valueProperty().addListener((observable) -> {
             changeDim();
         });
+        canvasWidth.textProperty().addListener((observable) -> {
+            if (isValid(canvasWidth.getText())) {
+                if (Integer.parseInt(canvasWidth.getText()) > 1440)
+                    canvasWidth.setText("1440");
+                else if (Integer.parseInt(canvasWidth.getText()) < 1)
+                    canvasWidth.setText("1");
+            }
+            else
+                canvasWidth.setText("1");
+        });
+        canvasHeight.textProperty().addListener((observable) -> {
+            if (isValid(canvasHeight.getText())) {
+                if (Integer.parseInt(canvasHeight.getText()) > 1440)
+                    canvasHeight.setText("1440");
+                else if (Integer.parseInt(canvasHeight.getText()) < 1)
+                    canvasHeight.setText("1");
+            }
+            else
+                canvasHeight.setText("1");
+        });
     }
 
     @FXML
     public void newCanvas(ActionEvent e) throws IOException {
-        //App.startSeparateWindow("NewCanvas", "Create New Canvas");
         canvasPane.setVisible(false);
         canvasPane.setDisable(true);
         newCanvasPane.setVisible(true);
         newCanvasPane.setDisable(false);
+        scene.setCursor(Cursor.DEFAULT);
+        selectedTool = "";
     }
 
     private void changeDim() {
@@ -132,6 +204,7 @@ public class MainController implements Initializable {
             canvas.setHeight(height);
             canvasBG.setWidth(width);
             canvasBG.setHeight(height);
+            canvasBG.setVisible(false);
 
             //  Change canvas location
             double paneMidpointX = canvasPane.getWidth()/2;
@@ -144,6 +217,8 @@ public class MainController implements Initializable {
             //  Clear canvas
             GraphicsContext context = canvas.getGraphicsContext2D();
             context.clearRect(0,0,canvas.getWidth(),canvas.getHeight());
+            brushTool.setFill(canvasColorpicker.getValue());
+            brushTool.fillRoundRect(0, 0, canvas.getWidth(), canvas.getHeight(), 1, 1);
 
             System.out.println("Canvas size: " + canvas.getWidth() + " x " + canvas.getHeight());
         }
@@ -170,10 +245,26 @@ public class MainController implements Initializable {
         return true;
     }
 
+    //  TOOLBAR METHODS
     @FXML
     private void brushSelected(ActionEvent e) {
-        selectedBrush = 1;
+        selectedTool = "brush";
+        scene.setCursor(Cursor.CROSSHAIR);
     }
+
+    @FXML
+    private void handSelected(ActionEvent e) {
+        selectedTool = "hand";
+        scene.setCursor(Cursor.OPEN_HAND);
+    }
+
+    @FXML
+    private void switchColor(ActionEvent e) {
+        Color temp = colorpickerPrimary.getValue();
+        colorpickerPrimary.setValue(colorpickerSecondary.getValue());
+        colorpickerSecondary.setValue(temp);
+    }
+
 
     @FXML
     private void switchToSecondary() throws IOException {
